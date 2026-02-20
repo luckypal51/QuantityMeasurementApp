548b8ced0b18459fa2ed02ddd672861218370cb1
Lucky Pal <pallucky936@gmail.com>
2026-02-20 17:04:26 +0530

    [Lucky Pal]: added Quantity Measurement App

--- src/main/java/measure/QuantityMeasurementApp.java
+++ src/main/java/measure/QuantityMeasurementApp.java
@@ -2,9 +2,14 @@ package measure;
 
 public class QuantityMeasurementApp {
 	public static  class FeetEquality {
-	     private double value;
-	     public FeetEquality() {};
+	     private final double value;
+	     public FeetEquality() {
+			this.value = 0;
+			}
 	     public FeetEquality(double value) {
+	    	 if(Double.isNaN(value)) {
+	    		 throw new IllegalArgumentException("Invalid Feet value");
+	    	 }
 	    	 this.value = value;
 	     }
 	     
@@ -12,11 +17,6 @@ public class QuantityMeasurementApp {
 	     public double getValue() {
 			return value;
 		}
-
-
-		 public void setValue(double value) {
-			 this.value = value;
-		 }
 
 
 		 @Override
@@ -31,9 +31,51 @@ public class QuantityMeasurementApp {
 		 }
 		 
 	}
+	
+	public static class Inches{
+		private final  double inche;
+		public Inches(double inche) {
+			if(Double.isNaN(inche)) {
+				throw new IllegalArgumentException("Invalid Inche value");
+			}
+			this.inche = inche;
+		}
+		
+		public Inches() {
+			this.inche = 0;
+		}
+		public double getInche() {
+			return inche;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(this==obj) {
+	    		return true;
+	    	}
+	    	if(obj==null||this.getClass()!=obj.getClass()) {
+	    		return false;
+	    	}
+	    	return Double.compare(this.inche, ((Inches) obj).getInche())==0;
+		 }
+	}
+	
+	  public static boolean demonstrateFeetEquality(double v1, double v2) {
+	        FeetEquality f1 = new FeetEquality(v1);
+	        FeetEquality f2 = new FeetEquality(v2);
+	        return f1.equals(f2);
+	    }
+
+	    public static boolean demonstrateInchesEquality(double v1, double v2) {
+	        Inches i1 = new Inches(v1);
+	        Inches i2 = new Inches(v2);
+	        return i1.equals(i2);
+	    }
+	
       public static void main(String[] args) {
 	   FeetEquality feet1 = new FeetEquality(1);
-	   FeetEquality feet2 = new FeetEquality(1);
+	   FeetEquality feet2 = new FeetEquality();
+	   System.out.println(feet1.equals(feet2));
 	   
     }
       
