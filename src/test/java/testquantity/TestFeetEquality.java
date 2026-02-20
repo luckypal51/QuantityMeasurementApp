8adaf957e0fc0651708bf1d0c74c7ea43c1bbb9e
Lucky Pal <pallucky936@gmail.com>
2026-02-20 14:38:08 +0530

    [Lucky Pal]: added test file

--- src/test/java/testquantity/TestFeetEquality.java
+++ src/test/java/testquantity/TestFeetEquality.java
@@ -1,5 +1,23 @@
 package testquantity;
 
-public class TestFeetEquality {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import measure.FeetEquality;
+
+public class TestFeetEquality {
+	FeetEquality feet1;
+	FeetEquality feet2;
+    @BeforeEach
+    public void setUp() {
+    	feet1 = new FeetEquality(1);
+    	feet2 = new FeetEquality(2);
+    }
+    
+    @Test
+    public void testEquals() {
+    	assertTrue(feet1.equals(feet2));
+    }
 }
