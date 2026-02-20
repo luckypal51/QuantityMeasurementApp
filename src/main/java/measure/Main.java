da47703c2b5bb77e88ddc0c72d379ae4fad7ffc9
Lucky Pal <pallucky936@gmail.com>
2026-02-20 14:38:30 +0530

    [Lucky Pal]: added Main File

---
+++ src/main/java/measure/Main.java
@@ -0,0 +1,13 @@
+package measure;
+
+import java.util.Scanner;
+
+public class Main {
+   
+    public static void main(String[] args) {
+    	 FeetEquality feet = new FeetEquality(1);
+    	    FeetEquality feet2 = new FeetEquality(1);
+    
+    	    System.out.println(feet.equals(feet2));
+	}
+}
