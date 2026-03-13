e13585f36780b520947f635bd26986d99a8040d3
Lucky Pal <pallucky936@gmail.com>
2026-03-13 16:38:25 +0530

    [Lucky Pal]: completed UC16-DatabaseIntegeration

--- src/main/java/measure/model/QuantityMeasurementEntity.java
+++ src/main/java/measure/model/QuantityMeasurementEntity.java
@@ -2,9 +2,13 @@ package measure.model;
 
 import java.util.Objects;
 
+import lombok.Getter;
+import lombok.Setter;
 import measure.entity.QuantityDTO;
 import measure.unit.IMeasurable;
 
+@Getter
+@Setter
 public class QuantityMeasurementEntity implements java.io.Serializable {
 		private static final long serialVersionUID = 1L;
 		public double thisValue;
@@ -29,6 +33,11 @@ public class QuantityMeasurementEntity implements java.io.Serializable {
 		// For capturing any error messages during operations
 		public String errorMessage;
 		
+		
+		public QuantityMeasurementEntity() {
+			super();
+		}
+
 		public QuantityMeasurementEntity(QuantityDTO thisQuantity,QuantityDTO  thatQuantity,String operation, String result) {
                 this(thisQuantity, thatQuantity, operation);
 				this.resultString = result;
@@ -76,6 +85,110 @@ public class QuantityMeasurementEntity implements java.io.Serializable {
 	                Objects.equals(resultString, that.resultString) &&
 	                Objects.equals(errorMessage, that.errorMessage);
 	    }
+        
+		public double getThisValue() {
+			return thisValue;
+		}
+
+		public void setThisValue(double thisValue) {
+			this.thisValue = thisValue;
+		}
+
+		public String getThisUnit() {
+			return thisUnit;
+		}
+
+		public void setThisUnit(String thisUnit) {
+			this.thisUnit = thisUnit;
+		}
+
+		public String getThisMeasurementType() {
+			return thisMeasurementType;
+		}
+
+		public void setThisMeasurementType(String thisMeasurementType) {
+			this.thisMeasurementType = thisMeasurementType;
+		}
+
+		public double getThatValue() {
+			return thatValue;
+		}
+
+		public void setThatValue(double thatValue) {
+			this.thatValue = thatValue;
+		}
+
+		public String getThatUnit() {
+			return thatUnit;
+		}
+
+		public void setThatUnit(String thatUnit) {
+			this.thatUnit = thatUnit;
+		}
+
+		public String getThatMeasurementType() {
+			return thatMeasurementType;
+		}
+
+		public void setThatMeasurementType(String thatMeasurementType) {
+			this.thatMeasurementType = thatMeasurementType;
+		}
+
+		public String getOperation() {
+			return operation;
+		}
+
+		public void setOperation(String operation) {
+			this.operation = operation;
+		}
+
+		public double getResultValue() {
+			return resultValue;
+		}
+
+		public void setResultValue(double resultValue) {
+			this.resultValue = resultValue;
+		}
+
+		public String getResultUnit() {
+			return resultUnit;
+		}
+
+		public void setResultUnit(String resultUnit) {
+			this.resultUnit = resultUnit;
+		}
+
+		public String getResultMeasurementType() {
+			return resultMeasurementType;
+		}
+
+		public void setResultMeasurementType(String resultMeasurementType) {
+			this.resultMeasurementType = resultMeasurementType;
+		}
+
+		public String getResultString() {
+			return resultString;
+		}
+
+		public void setResultString(String resultString) {
+			this.resultString = resultString;
+		}
+
+		public boolean isError() {
+			return isError;
+		}
+
+		public void setError(boolean isError) {
+			this.isError = isError;
+		}
+
+		public String getErrorMessage() {
+			return errorMessage;
+		}
+
+		public void setErrorMessage(String errorMessage) {
+			this.errorMessage = errorMessage;
+		}
 
 		@Override
 		public String toString() {
--- src/main/java/measure/reposistory/IQuantityMeasurementRepository.java
+++ src/main/java/measure/reposistory/IQuantityMeasurementRepository.java
@@ -1,9 +1,18 @@
 package measure.reposistory;
 
 import measure.model.QuantityMeasurementEntity;
+
+import java.sql.ResultSet;
 import java.util.*;
 
 public interface IQuantityMeasurementRepository {
       void save(QuantityMeasurementEntity enity);
       List<QuantityMeasurementEntity> getAllMeasurement();
+      List<QuantityMeasurementEntity> getMeasurementByOperation(String Operation);
+      List<QuantityMeasurementEntity> getMeasurementByType(String type);
+      int getTotalCount();
+      void deleteALl();
+      String getPoolStatistic();
+      void releaseResource();
+     
 }
