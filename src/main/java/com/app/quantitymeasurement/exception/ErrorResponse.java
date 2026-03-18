92c6c0e43f12198822243b087f4199490e2f84df
Lucky Pal <pallucky936@gmail.com>
2026-03-18 20:47:54 +0530

    [Lucky Pal]: completed Spring Integeration UC17

---
+++ src/main/java/com/app/quantitymeasurement/exception/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.app.quantitymeasurement.exception;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+	public LocalDateTime timestamp;
+	public int status;
+	public String error;
+	public String message;
+	public String path;
+}
