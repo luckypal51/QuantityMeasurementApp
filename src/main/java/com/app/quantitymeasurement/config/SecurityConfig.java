32b4557d7537c8814e325386e4f8e998c5374656
Lucky Pal <pallucky936@gmail.com>
2026-04-04 13:24:37 +0530

    [Lucky Pal]: added Security Configuration

--- src/main/java/com/app/quantitymeasurement/config/SecurityConfig.java
+++ src/main/java/com/app/quantitymeasurement/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     UrlBasedCorsConfigurationSource corsConfigurationSource() {
 	CorsConfiguration configuration = new CorsConfiguration();
-	configuration.setAllowedOrigins(Arrays.asList( "http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:5500","http://localhost:4200")
+	configuration.setAllowedOrigins(Arrays.asList( "http://localhost:3000", "http://localhost:8080", "http://127.0.0.1:5500","http://localhost:4200","https://quantitymeasurementapp-production-218f.up.railway.app")
 	);
 	configuration. setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
